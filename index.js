3b105f34f60201bff93efa0becab53d0215a1c06
Ryutaro0802 <ne200152g@gmail.com>
2020-06-30 21:55:07 +0900

    クロスドメインを許可

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const releaseInfoSingle = require('./api/releaseInfo')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/api/v1/releaseInfo/single", releaseInfoSingle.single)
 
 app.get("/api/v1/releaseInfo/album", releaseInfoSingle.album)

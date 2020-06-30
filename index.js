d7014269c37ac0ecc39e3a8ae89e98e00f081861
Ryutaro0802 <ne200152g@gmail.com>
2020-06-30 17:45:15 +0900

    画像のパスを追加

--- index.js
+++ index.js
@@ -15,30 +15,35 @@ app.get("/api/v1/releaseInfo", function (req, res) {
       title: "人生ふたり坂",
       artist: "内川ひろ美",
       releaseDate: "2020/07/03",
+      migSrc: "https://m.media-amazon.com/images/I/318h4iu0jKL._SL160_.jpg",
       price: "1,200円"
     },
     {
       title: "リヴィング・イン・ア・ゴースト・タウン",
-      artist: "内川ひろ美",
+      artist: "ザ・ローリング・ストーンズ",
       releaseDate: "2020/07/03",
+      migSrc: "https://m.media-amazon.com/images/I/51Mv2OA7hAL._SL160_.jpg",
       price: "2,420円"
     },
     {
       title: "向日葵～美美津より～",
       artist: "福田真悠実",
       releaseDate: "2020/07/01",
+      migSrc: "https://m.media-amazon.com/images/I/51tqSm8q6sL._SL160_.jpg",
       price: "1,300円"
     },
     {
       title: "大衆は我々を裁けない(Type B)",
       artist: "SHIVA",
       releaseDate: "2020/07/01",
+      migSrc: "https://m.media-amazon.com/images/I/61kkT939T-L._SL160_.jpg",
       price: "1,650円"
     },
     {
       title: "THE IDOLM@STER CINDERELLA MASTER 3chord for the Rock!",
       artist: "ゲーム・ミュージック",
       releaseDate: "2020/07/01",
+      migSrc: "https://m.media-amazon.com/images/I/61Bvjt7JMJL._SL160_.jpg",
       price: "1,540円"
     }
   ])
